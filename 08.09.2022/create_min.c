6d955287eb1eb86dd3582ac858060237a9655ffd
Ksenob1te <63819958+Ksenob1te@users.noreply.github.com>
2022-09-15 12:02:28 +0300

    Update create_min.c

--- 08.09.2022/create_min.c
+++ 08.09.2022/create_min.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 
-// функция, для подсчета количества вхождения цифры в числе
 int count_elements(int n, int element) {
     int ans = 0;
     while (n > 0) {
@@ -12,35 +11,40 @@ int count_elements(int n, int element) {
     return ans;
 }
 
-
-int main() {
-    int n;
-    // получаем значение n из потока ввода (%d - означает, что ввод состоит только из цифры)
-    scanf("%d", &n);
-    // создаем переменные, которые будем использовать в коде
-    // в x будем хранить копию введенного n
-    // в min_local сохраним минимальную ненулевую цифру из исходного числа
-    int x = n;
+int find_first(int x) {
     int min_local = 10;
-    // найдем min_local
     while (x > 0) {
         if (x % 10 != 0 && x % 10 < min_local)
             min_local = x % 10;
         x /= 10;
     }
-    int ans = min_local;
+    return min_local;
+}
 
-    // теперь для каждой возможной цифры посчитаем количество вхождений ее в начальное число
-    // и прибавим их с соответствующим разрядом в порядке возрастания
+
+void fill_result(const int* min_local, int* ans, const int* n) {
     for (int i = 0; i < 10; ++i) {
-        int new_iterator = count_elements(n, i);
-        if (i == min_local)
-            new_iterator -= 1;  // не забудем, что одно ненулевое число мы уже добавили в ответ
+        int new_iterator = count_elements(*n, i);
+        if (i == *min_local)
+            new_iterator -= 1;
         for (int j = 0; j < new_iterator; ++j) {
-            ans = ans * 10 + i;
+            *ans = *ans * 10 + i;
         }
     }
-    // выводим ответ
-    printf("%d", ans);
+    printf("%d", *ans);
+}
+
+
+int main() {
+    int n = 0;
+    int scan_result = scanf("%d", &n);
+    if (scan_result <= 0) {
+        printf("input_fail\n");
+        return 0;
+    }
+    int min_local = find_first(n);
+    int ans = min_local;
+
+    fill_result(&min_local, &ans, &n);
     return 0;
 }
