bbd0532c7e863e33420c336a61952a3cdca94e1f
Ksenob1te <63819958+Ksenob1te@users.noreply.github.com>
2022-09-29 16:53:13 +0300

    Update count_sin.c

--- 22.09.2022/count_sin.c
+++ 22.09.2022/count_sin.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-double count_row(const double* x, const int* n) {
-    double xSquared = *x * *x;
-    double answer = *x, mult = *x;
-    for (int i = 3; i <= *n; i += 2) {
+double count_row(const double x, const int n) {
+    double xSquared = x * x;
+    double answer = x, mult = x;
+    for (int i = 3; i <= n; i += 2) {
         mult = -mult * (xSquared / (i * (i - 1)));
         answer += mult;
     }
@@ -15,11 +15,11 @@ int main() {
     int n = 0;
     printf("Type x and n\n");
     int input_status = scanf("%lf%d", &x, &n);
-    if (input_status <= 1) {
+    if (input_status <= 0) {
         printf("Incorrect Input");
         return 0;
     }
     // lg(2 ^ 52) == 15
-    printf("%.15f", count_row(&x, &n));
+    printf("%.15f", count_row(x, n));
     return 0;
 }
